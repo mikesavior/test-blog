0c4459eaea3197b1c365b1efd859069710cff907
Kaz Archer <you@example.com>
2024-12-22 23:46:01 -0500

    fix: Update fetch URL to use relative path for posts API

--- src/components/posts/PostsList.js
+++ src/components/posts/PostsList.js
@@ -9,7 +9,7 @@ function PostsList() {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/posts');
+        const response = await fetch('api/posts');
         const data = await response.json();
         
         if (!response.ok) {
