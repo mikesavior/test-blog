1975db20eb5e2e129fcacb0151d4650e400c0fb6
Kaz Archer <you@example.com>
2024-12-22 22:39:17 -0500

    fix: Remove environment variable for PORT and hardcode 5000

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const sequelize = require('./config/database');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Database sync
 sequelize.sync({ alter: true })
